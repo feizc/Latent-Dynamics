7fa5f4792aea6559738f6c6ed6ff314dd69b38e7
费政聪 <3300949577@qq.com>
2022-04-19 20:57:28 +0800

    build framework

---
+++ dataset.py
@@ -0,0 +1,39 @@
+import json 
+
+
+
+
+def data_preprocess(data_path): 
+    with open(data_path, 'r', encoding='utf-8') as f: 
+        data_dict = json.load(f) 
+    image_url_dict = {} 
+    for item in data_dict['images']: 
+        image_id = item['id'] 
+        for key in item.keys(): 
+            if 'url' in key:
+                image_url_dict[image_id] = item[key]
+    
+    story_dict = {}
+    for item in data_dict['annotations']: 
+        item = item[0]
+        story_id = item['story_id'] 
+        if story_id not in story_dict.keys(): 
+            story_dict[story_id] = {}
+            story_dict[story_id]['url'] = [] 
+            story_dict[story_id]['txt'] = [] 
+        image_id = item['photo_flickr_id'] 
+        if image_id not in image_url_dict.keys():
+            continue 
+        story_dict[story_id]['url'].append(image_url_dict[image_id]) 
+        story_dict[story_id]['txt'].append(item['text']) 
+    
+    with open('./dataset/train.json', 'w', encoding='utf-8') as f: 
+        json.dump(story_dict, f, indent=4)
+
+
+
+
+if __name__ == "__main__":
+    data_path = './dataset/VIST/sis/val.story-in-sequence.json' 
+
+    data_preprocess(data_path)
